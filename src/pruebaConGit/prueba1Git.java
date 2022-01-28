a138821c13d8923313db0aaefc01aa0aa8c63358
victorgarcia00 <victor.garcia@juanxxiii.net>
2022-01-28 19:16:28 +0100

    agregando variables

--- src/pruebaConGit/prueba1Git.java
+++ src/pruebaConGit/prueba1Git.java
@@ -4,7 +4,9 @@ public class prueba1Git {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hola jaime y liviu");
+		String nombre ="pedro jimenez";
 	}
+	
 
 }
